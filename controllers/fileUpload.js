d93afce656fcd1e2a98fb89f46879150babd512b
Inam10 <inamm94@gmail.com>
2023-12-22 16:08:49 +0530

    Image and vedio upload api complete

--- controllers/fileUpload.js
+++ controllers/fileUpload.js
@@ -30,9 +30,15 @@ function isFileTypeSpported(type, supportedTypes) {
   return supportedTypes.includes(type);
 }
 
-async function uploadFileToCloudinary(file, folder) {
+async function uploadFileToCloudinary(file, folder ,quality) {
     try {
       const options = { folder };
+      console.log("temp file path" , file.tempFilePath);
+
+      if(quality){
+        options.quality = quality;
+      }
+      options.resource_type = "auto";
       return await cloudinary.uploader.upload(file.tempFilePath, options);
     } catch (error) {
       throw new Error(`Error uploading file to Cloudinary: ${error.message}`);
@@ -66,12 +72,12 @@ exports.imageUpload = async (req, res) => {
     const response = await uploadFileToCloudinary(file, "InamsCode");
     console.log("response", response);
     //Entry save in db
-    // const fileData = await File.create({
-    //   name,
-    //   tags,
-    //   email,
-    //   imageUrl,
-    // });
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl:response.secure_url,
+    });
     res.json({
       success: true,
       message: "Image successfully uploaded",
@@ -86,3 +92,106 @@ exports.imageUpload = async (req, res) => {
     });
   }
 };
+
+
+
+
+
+//Handler for vedio upload here
+exports.vedioUpload = async (req,res)=>{
+  try{
+    //data fetch
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.vedioFile;
+
+    //validations
+    const supportedTypes = ["mp4", "mov"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSpported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format is not supported",
+      });
+    }  
+    
+    //file format is supported
+    console.log("upload to inamsCode");
+    const response = await uploadFileToCloudinary(file, "InamsCode");
+    console.log("response", response);
+
+     //Entry save in db
+     const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl:response.secure_url,
+    });
+
+    res.json({
+      success: true,
+      message: "Vedio successfully uploaded",
+      imageUrl: response.secure_url,
+    });
+
+
+  }
+  catch(error){
+    res.status(400).json({
+      success:false,
+      message:"something went wrong here."
+    })
+  }
+}
+
+
+
+// -------------------------------------------------------------
+
+exports.imageSizeReducer = async(req,res) =>{
+  try{
+    //data fetch
+    const { name, tags, email } = req.body;
+    console.log(name, tags, email);
+
+    const file = req.files.imageFile;
+    console.log(file);
+
+    //validations
+    const supportedTypes = ["jpg", "jpeg", "png"];
+    const fileType = file.name.split(".")[1].toLowerCase();
+
+    if (!isFileTypeSpported(fileType, supportedTypes)) {
+      return res.status(400).json({
+        success: false,
+        message: "File format is not supported",
+      });
+    }
+
+    //file format is supported
+    console.log("upload to inamsCode");
+    const response = await uploadFileToCloudinary(file, "InamsCode",90);
+    console.log("response", response);
+    //Entry save in db
+    const fileData = await File.create({
+      name,
+      tags,
+      email,
+      imageUrl:response.secure_url,
+    });
+    res.json({
+      success: true,
+      message: "Image successfully uploaded",
+      imageUrl: response.secure_url,
+    });
+ 
+  }
+  catch(error){
+    res.status(400).json({
+      success:false,
+      message:"something went wrong here."
+    })
+  }
+}
--- routes/FileUpload.js
+++ routes/FileUpload.js
@@ -1,12 +1,17 @@
 const express = require("express");
 const router = express.Router();
 
-const {imageUpload , vedioUpload , imageReduerUpload , localFileUpload} = require("../controllers/fileUpload");
-
+const {
+  imageUpload,
+  vedioUpload,
+  imageSizeReducer,
+  localFileUpload,
+} = require("../controllers/fileUpload");
 
 //api routes
-router.post("/localFileUpload" , localFileUpload);
-router.post("/imageUpload" , imageUpload);
-
+router.post("/localFileUpload", localFileUpload);
+router.post("/imageUpload", imageUpload);
+router.post("/vedioUpload", vedioUpload);
+router.post("/imageSizeReducer" , imageSizeReducer);
 
 module.exports = router;
