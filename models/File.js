ac67ee4f778eca8e5615b9f348173fa225ec375b
Inam10 <inamm94@gmail.com>
2023-12-22 19:39:19 +0530

    Complete this project sending mail successfully here

--- models/File.js
+++ models/File.js
@@ -1,20 +1,49 @@
- const mongoose = require("mongoose");
+const mongoose = require("mongoose");
+const nodemailer = require("nodemailer");
 
- const fileSchema = new mongoose.Schema({
-    name :{
-    type : String,
-    required :true
-    } ,
-    imageUrl:{
-        type : String,
-    },
-    tags: {
-        type : String
-    },
-    email : {
-        type : String
-    }
- });
+const fileSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  imageUrl: {
+    type: String,
+  },
+  tags: {
+    type: String,
+  },
+  email: {
+    type: String,
+  },
+});
 
- const File = mongoose.model("File" , fileSchema);
- module.exports = File;
+//post middleware here
+fileSchema.post("save", async function (doc) {
+  try {
+    console.log("doc", doc);
+
+    //transoporter
+    let transoporter = nodemailer.createTransport({
+      host: process.env.MAIL_HOST,
+      auth: {
+        user: process.env.MAIL_USER,
+        pass: process.env.MAIL_PASS,
+      },
+    });
+
+    //send mail
+    let info = await transoporter.sendMail({
+      form: `Inams Coding`,
+      to: doc.email,
+      subject: "New file Uploaded on Coudinary",
+      html: `<h2>Hello user</h2> <p>File uploaded. view here <a href="${doc.imageUrl}">${doc.imageUrl}</a></p>`,
+    });
+
+    console.log("Info", info);
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+const File = mongoose.model("File", fileSchema);
+module.exports = File;
